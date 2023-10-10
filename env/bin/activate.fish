c7c69b760b88bf8a49eb5dde6154e8e9f86424b1
Higor Marcelino <higor.freitas@aluno.faculdadeimpacta.com.br>
2023-10-09 21:01:54 -0300

    fix
    fix test

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/higor/ac03-automocao-testes-de-soft/env"
+set -gx VIRTUAL_ENV "/home/higor/Documentos/GitHub/ac03-automocao-testes/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
